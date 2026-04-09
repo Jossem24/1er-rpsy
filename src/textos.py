f39779bff5cbf929fa6666867e9dd6c3e3bd2193
Jossem24 <paparilo024@hotmail.com>
2026-04-09 14:58:53 -0300

    atualizando textos

--- src/textos.py
+++ src/textos.py
@@ -8,7 +8,31 @@ print(texto)
 
 
 
-email = "PAPARILO@GMAIL.COM"
+email = "PAPArilo@GMAIL.COM"
 
-email = email.lower()
+email = email.upper()
+print(email)
+
+
+
+gano = 45000
+gasto = 23500
+
+gano_final = gano - gasto
+margem = gano_final / gano
+
+texto = f"O lucro foi de {gano_final:.2f} e a margem foi de {margem:.0%}"
+print(texto)
+
+
+
+
+foco = email.find("@")
+print(foco)
+
+lugar = email[:foco]
+print(lugar)
+
+
+email = email.replace("@GMAIL.COM", "@yahoo.com")
 print(email)
