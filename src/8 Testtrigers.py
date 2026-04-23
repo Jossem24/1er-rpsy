e215b415133806ecd77452bcff3a996e7b0ed422
Jossem24 <paparilo024@hotmail.com>
2026-04-22 23:52:18 -0300

    Começando rutina

--- src/8 Testtrigers.py
+++ src/8 Testtrigers.py
@@ -1,36 +1,24 @@
+#Exercicios Ponto 4
 
+#4.1
+nome = input("Qual é seu nome? ")
+cidade = input("Onde mora? ")
+print("Olá,",nome, "Voce mora em", cidade )
 
+#4.2
+numero_1 = int(input("Digite um numero: "))
+numero_2 = int(input("Digite outro numero: "))
+print("A soma deles é", numero_1 + numero_2)
+print("A subs deles é", numero_1 - numero_2)
+print("A multiplicação deles é", numero_1 * numero_2)
 
+#4.3
+nome_aluno = input("Digite seu nome: ")
+print("Olá aluno, ", nome_aluno, "Vamos mostrar suas notas")
 
-
-def escoger():
-    samsung = 1
-    iphone = 2
-    outro = 3
-
-    while True:
-
-            try: 
-                
-                print("1- Samsung", "2- Iphone", "3-Outro")
-                telefono = int(input("Cual es tu movil?"))
-
-                if  telefono == 1 :
-                    print("Eres pituco")
-                    break
-                elif telefono == 2 :
-                    print ("Estás en algo")
-                    break
-                elif telefono == 3:    
-                    print("Fuera misio") 
-                    break
-                else:
-                     print("Opcao invalida")
-                     
-            except ValueError:
-                    print("Tenta de novo")
-                    
-
-            
-
-escoger()
+nota_1 = int(input("De quanto foi sua primeira nota: "))
+nota_2 = int(input("De quanto foi sua segunda nota: "))
+media_final = (nota_1 + nota_2) / 2
+print("Suas notas foram", nota_1, "e",nota_2)
+print("Sua media foi", (nota_1 + nota_2) / 2)
+print("Sua media final foi", media_final)
