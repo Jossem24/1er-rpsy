84e779aaa16948f4af2e016db34301d03f44bc1b
Jossem24 <paparilo024@hotmail.com>
2026-04-28 00:53:36 -0300

    Atualizando 28

---
+++ src/olhe.py
@@ -0,0 +1,24 @@
+edade = int(input("Qual é a sua idade? :"))
+
+if edade >= 18:
+    print("Vocé é maior de edade")
+else :
+    print("Vocé é menor de edade")
+
+
+nota = int(input("Qual é a sua nota"))
+
+if nota >= 7:
+   print("Aprovado")
+else :
+   print("Reprovado") 
+
+
+cali = int(input("Cuanto tirou na prova? : "))
+
+if cali >= 9:
+   print("Excelente")
+elif cali >= 7 :
+   print("Aprovado") 
+else :  
+   print("Reeprovado")     
