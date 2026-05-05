45b5b2cbbd95f088539eb915e75b2b9177811fe4
Jossem24 <paparilo024@hotmail.com>
2026-05-05 11:21:01 -0300

    mini proj

--- src/9 Pontos.py
+++ src/9 Pontos.py
@@ -1,114 +1,114 @@
-# #Exercicios Ponto 4
+#Exercicios Ponto 4
 
-# #4.2
-# nome = input("Qual é seu nome? ")
-# cidade = input("Onde mora? ")
-# print("Olá,",nome, "Voce mora em", cidade )
+#4.2
+nome = input("Qual é seu nome? ")
+cidade = input("Onde mora? ")
+print("Olá,",nome, "Voce mora em", cidade )
 
-# #4.3
-# numero_1 = int(input("Digite um numero: "))
-# numero_2 = int(input("Digite outro numero: "))
-# print("A soma deles é", numero_1 + numero_2)
-# print("A subs deles é", numero_1 - numero_2)
-# print("A multiplicação deles é", numero_1 * numero_2)
+#4.3
+numero_1 = int(input("Digite um numero: "))
+numero_2 = int(input("Digite outro numero: "))
+print("A soma deles é", numero_1 + numero_2)
+print("A subs deles é", numero_1 - numero_2)
+print("A multiplicação deles é", numero_1 * numero_2)
 
-# #4.4
-# nome_aluno = input("Digite seu nome: ")
-# print("Olá aluno, ", nome_aluno, "Vamos mostrar suas notas")
+#4.4
+nome_aluno = input("Digite seu nome: ")
+print("Olá aluno, ", nome_aluno, "Vamos mostrar suas notas")
 
-# nota_1 = int(input("De quanto foi sua primeira nota: "))
-# nota_2 = int(input("De quanto foi sua segunda nota: "))
-# media_final = (nota_1 + nota_2) / 2
-# print("Suas notas foram", nota_1, "e",nota_2)
-# print("Sua media foi", (nota_1 + nota_2) / 2)
-# print("Sua media final foi", media_final)
-
-
-# #4.5
-# n_1 = int(input("Digite um numero: "))
-# n_2 = int(input("Digite um numero: "))
-# print(n_1 + n_2)
-# print(n_1 - n_2)
-# print(n_1 * n_2)
-# print(n_1 / n_2)
+nota_1 = int(input("De quanto foi sua primeira nota: "))
+nota_2 = int(input("De quanto foi sua segunda nota: "))
+media_final = (nota_1 + nota_2) / 2
+print("Suas notas foram", nota_1, "e",nota_2)
+print("Sua media foi", (nota_1 + nota_2) / 2)
+print("Sua media final foi", media_final)
 
 
-# idade = int(input("Digite sua idade: "))
-# print(idade >= 18)
-# print(idade < 18)
+#4.5
+n_1 = int(input("Digite um numero: "))
+n_2 = int(input("Digite um numero: "))
+print(n_1 + n_2)
+print(n_1 - n_2)
+print(n_1 * n_2)
+print(n_1 / n_2)
 
 
-# doc = input("Tem documento? :").lower()   #lower para se o usuario escrever sim de qualquer jeito
-# adulto = int(input("Dame tu edad chibolo: "))
-# print( adulto >= 18 and doc == "sim" )
+idade = int(input("Digite sua idade: "))
+print(idade >= 18)
+print(idade < 18)
 
 
-# #4.6
-# edade = int(input("Qual é a sua idade? :"))
-# if edade >= 18:
-#     print("Vocé é maior de edade")
-# else :
-#     print("Vocé é menor de edade")
+doc = input("Tem documento? :").lower()   #lower para se o usuario escrever sim de qualquer jeito
+adulto = int(input("Dame tu edad chibolo: "))
+print( adulto >= 18 and doc == "sim" )
 
-# nota = int(input("Qual é a sua nota"))
-# if nota >= 7:
-#    print("Aprovado")
-# else :
-#    print("Reprovado") 
 
-# cali = int(input("Cuanto tirou na prova? : "))
-# if cali >= 9:
-#    print("Excelente")
-# elif cali >= 7 :
-#    print("Aprovado") 
-# else :  
-#    print("Reeprovado")     
+#4.6
+edade = int(input("Qual é a sua idade? :"))
+if edade >= 18:
+    print("Vocé é maior de edade")
+else :
+    print("Vocé é menor de edade")
 
-#4.7 
-# for a in range(1, 6): 
-#     print( a )
+nota = int(input("Qual é a sua nota"))
+if nota >= 7:
+   print("Aprovado")
+else :
+   print("Reprovado") 
 
-# for b in range(3):
-#     print("python")
+cali = int(input("Cuanto tirou na prova? : "))
+if cali >= 9:
+   print("Excelente")
+elif cali >= 7 :
+   print("Aprovado") 
+else :  
+   print("Reeprovado")     
 
-# numero = 1
-# while numero <= 5:
-#     print(numero)
-#     numero = numero + 1
+4.7 
+for a in range(1, 6): 
+    print( a )
 
-# #4.8 
-# for numero in range(1, 6):
-#     if numero == 4:
-#        break
+for b in range(3):
+    print("python")
+
+numero = 1
+while numero <= 5:
+    print(numero)
+    numero = numero + 1
+
+#4.8 
+for numero in range(1, 6):
+    if numero == 4:
+       break
        
-#     print(numero)
+    print(numero)
 
-# for i in range(1, 6):
-#     if i == 3:
+for i in range(1, 6):
+    if i == 3:
      
-#      continue  
+     continue  
 
-#     print(i)  
-
-
-# for p in range(1, 6):
-#     if p == 2:
-#         pass
-
-#     print(p)
+    print(i)  
 
 
-# #4.9
-# eu = "jose jose seminario carrion"
-# print(eu)
+for p in range(1, 6):
+    if p == 2:
+        pass
 
-# pala = "python"
-# print(pala[0])
-# print(pala[5])
+    print(p)
 
-# var = input("Dime una palavra: ")
-# print(var.lower())
-# print(var.upper())
+
+#4.9
+eu = "jose jose seminario carrion"
+print(eu)
+
+pala = "python"
+print(pala[0])
+print(pala[5])
+
+var = input("Dime una palavra: ")
+print(var.lower())
+print(var.upper())
 
 
 
--- src/Testing.py
+++ src/Testing.py
@@ -1,10 +1,37 @@
-eu = "jose jose seminario carrion"
-print(eu)
+def cadastro_1():
+    while True :
+        senha = int(input("Digite sua senha: "))
+        if senha == 1234 :
+            print("Acceso liberado")
+            break
+        else :
+            print("Digite novamente")        
+    return      
+cadastro_1() 
 
-pala = "python"
-print(pala[0])
-print(pala[5])
+def dados_pessoais():
 
-var = input("Dime una palavra: ")
-print(var.lower())
-print(var.upper())
+    nome = input("Qual é teu nome: ").lower()
+    cidade =input("Em que cidade voce mora: ")
+    idade =input("Qual é a tua idade: ")
+    nota_1 = int(input("Fala sua nota 1: "))
+    nota_2 = int(input("Fala sua nota 2: "))
+    media_final = int((nota_1 + nota_2) / 2)
+
+    print(nome)
+    print(nome.upper())
+    print(cidade)
+    print(nota_1 + nota_2)
+    print((nota_1 + nota_2) / 2)
+
+    if media_final >= 9 :
+        print("Excelente") 
+        
+    elif media_final >= 7:
+        print("Aprovado")
+    else :
+        print("Reprovado")       
+            
+    return None
+
+dados_pessoais()
