2a484f5e07db7775af06644283674ceeea20dd4a
Jossem24 <paparilo024@hotmail.com>
2026-07-17 09:07:18 -0300

    http-comunication

--- src/6Testing.py
+++ src/6Testing.py
@@ -1,5 +1,4 @@
 import requests
 
-resposta = requests.get("https://jsonplaceholder.typicode.com/posts/1")
+resposta = requests.delete("https://jsonplaceholder.typicode.com/posts/1")
 print(resposta.status_code)
-print(resposta.json())
