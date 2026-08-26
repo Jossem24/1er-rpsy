98db10fb76d5f40714b0f349d54ba9de1e5e2639
Jossem24 <paparilo024@hotmail.com>
2026-08-26 14:32:41 -0300

    crud completo

--- src/2-TESTEO.py
+++ src/2-TESTEO.py
@@ -51,6 +51,48 @@ def usuarios():
 
       return render_template("usuarios.html", dados=dados) 
 
+@app.route("/editar/<int:id_usuario>", methods=["GET", "POST"])
+def editar(id_usuario):
+
+      conexao = sqlite3.connect("crud.db")
+      cursor = conexao.cursor()
+
+
+      if request.method == "POST":
+         nome = request.form["nome"]
+         idade = request.form["idade"]
+         cursor.execute(
+              "UPDATE usuarios set nome = ?, idade = ? WHERE id = ? ",
+              (nome, idade, id_usuario,),
+         )
+         conexao.commit()
+         conexao.close()
+         return redirect(url_for("usuarios"))
+
+      cursor.execute(
+            "SELECT id, nome, idade FROM usuarios WHERE id = ?",
+            (id_usuario,),       
+      )
+      dados = cursor.fetchone()
+      conexao.close()
+      return render_template("editar.html", dados=dados)
+
+@app.route("/apagar/<id_usuario>", methods=["POST"])
+def apagar(id_usuario):
+
+     conexao=sqlite3.connect("crud.db")
+     cursor=conexao.cursor()
+
+     cursor.execute(
+          "DELETE FROM usuarios WHERE id=?",
+          (id_usuario,),
+     )
+
+     conexao.commit()
+     conexao.close()
+     return redirect(url_for("usuarios"))
+
+           
 
 if __name__ == "__main__":
       banco_novo()
