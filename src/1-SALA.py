d92401649ca64dd70c60d22c9b5ee35f107830c1
Jossem24 <paparilo024@hotmail.com>
2026-09-16 14:30:40 -0300

    validação

--- src/1-SALA.py
+++ src/1-SALA.py
@@ -1135,3 +1135,107 @@ if __name__ == "__main__":
 #  </body>
 # </html>
 
+#Ponto 14
+
+import sqlite3
+from flask import Flask, redirect, url_for, render_template, request
+
+app = Flask(__name__)
+
+#função para criar o banco, se executa alem do app.run que é para as rotas
+def banco_novo():       
+    conexao = sqlite3.connect("crud.db")
+    cursor = conexao.cursor()
+
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS usuarios "
+        "(id INTEGER PRIMARY KEY AUTOINCREMENT,"
+        "nome TEXT,"
+        "idade INTEGER)"
+    )
+
+    conexao.commit()
+    conexao.close()
+
+@app.route("/cadastro", methods=["GET", "POST"])
+def cadastro():
+          if request.method == "POST":
+            nome = request.form["nome"]
+            idade = request.form["idade"]
+
+            conexao = sqlite3.connect("crud.db")
+            cursor = conexao.cursor()
+
+            cursor.execute(
+
+                  "INSERT INTO usuarios (nome, idade) VALUES (?,?)",
+                  (nome, idade),
+            )
+            conexao.commit()
+            conexao.close()
+
+            return redirect(url_for("usuarios"))
+          return render_template("cadastro.html")
+
+#a ruta serve para acessar aos usuarios
+@app.route("/usuarios")   
+def usuarios():
+      conexao = sqlite3.connect("crud.db")
+      cursor = conexao.cursor()
+
+      cursor.execute(
+                " SELECT id, nome, idade FROM usuarios "        
+      )
+      dados = cursor.fetchall()
+      conexao.close()
+
+      return render_template("usuarios.html", dados=dados) 
+
+# as / separam endereços 
+# <> e o valor duma var que vem da URL, int indica o tipo de dado e : separa isso 
+# entender porque é :id_usuario
+#entende ro uso da virgula no (id_usuario,),
+@app.route("/editar/<int:id_usuario>", methods=["GET", "POST"])
+def editar(id_usuario):
+
+      conexao = sqlite3.connect("crud.db")
+      cursor = conexao.cursor()
+
+
+      if request.method == "POST":
+         nome = request.form["nome"]
+         idade = request.form["idade"]
+         cursor.execute(
+              "UPDATE usuarios set nome = ?, idade = ? WHERE id = ? ",
+              (nome, idade, id_usuario,),
+         )
+         conexao.commit()
+         conexao.close()
+         return redirect(url_for("usuarios"))
+
+      cursor.execute(
+            "SELECT id, nome, idade FROM usuarios WHERE id = ?",
+            (id_usuario,),       
+      )
+      dados = cursor.fetchone()
+      conexao.close()
+      return render_template("editar.html", dados=dados)
+
+@app.route("/apagar/<id_usuario>", methods=["POST"])
+def apagar(id_usuario):
+
+     conexao=sqlite3.connect("crud.db")
+     cursor=conexao.cursor()
+
+     cursor.execute(
+          "DELETE FROM usuarios WHERE id=?",
+          (id_usuario,),
+     )
+
+     conexao.commit()
+     conexao.close()
+     return redirect(url_for("usuarios"))
+
+if __name__ == "__main__":
+      banco_novo()
+      app.run()
--- src/2-TESTEO.py
+++ src/2-TESTEO.py
@@ -1,99 +1,21 @@
-import sqlite3
-from flask import Flask, redirect, url_for, render_template, request
-
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
-def banco_novo():
-    conexao = sqlite3.connect("crud.db")
-    cursor = conexao.cursor()
-
-    cursor.execute(
-        "CREATE TABLE IF NOT EXISTS usuarios "
-        "(id INTEGER PRIMARY KEY AUTOINCREMENT,"
-        "nome TEXT,"
-        "idade INTEGER)"
-    )
-
-    conexao.commit()
-    conexao.close()
-
-@app.route("/cadastro/", methods=["GET", "POST"])
+@app.route("/cadastro", methods=["GET", "POST"])
 def cadastro():
-          if request.method == "POST":
-            nome = request.form["nome"]
-            idade = request.form["idade"]
+    if request.method == "POST":
+      nome = request.form.get("nome", "").strip()
+      idade_texto = request.form.get("idade", "").strip()
 
-            conexao = sqlite3.connect("crud.db")
-            cursor = conexao.cursor()
+      if not nome or not idade_texto:
+        mensagem = "Digite un nome"
+        return render_template("cadastro.html", mensagem=mensagem)
 
-            cursor.execute(
+      try :
+         idade = int(idade_texto)
+      except ValueError:
+         mensagem = "A idade precisa ser um numero"
+         return render_template("cadastro.html", mensagem=mensagem)
+    return render_template("cadastro.hmtl")
 
-                  "INSERT INTO usuarios (nome, idade) VALUES (?,?)",
-                  (nome, idade),
-            )
-            conexao.commit()
-            conexao.close()
-
-            return redirect(url_for("usuarios"))
-          return render_template("cadastro.html")
-
-@app.route("/usuarios")
-def usuarios():
-      conexao = sqlite3.connect("crud.db")
-      cursor = conexao.cursor()
-
-      cursor.execute(
-                " SELECT id, nome, idade FROM usuarios "        
-      )
-      dados = cursor.fetchall()
-      conexao.close()
-
-      return render_template("usuarios.html", dados=dados) 
-
-@app.route("/editar/<int:id_usuario>", methods=["GET", "POST"])
-def editar(id_usuario):
-
-      conexao = sqlite3.connect("crud.db")
-      cursor = conexao.cursor()
-
-
-      if request.method == "POST":
-         nome = request.form["nome"]
-         idade = request.form["idade"]
-         cursor.execute(
-              "UPDATE usuarios set nome = ?, idade = ? WHERE id = ? ",
-              (nome, idade, id_usuario,),
-         )
-         conexao.commit()
-         conexao.close()
-         return redirect(url_for("usuarios"))
-
-      cursor.execute(
-            "SELECT id, nome, idade FROM usuarios WHERE id = ?",
-            (id_usuario,),       
-      )
-      dados = cursor.fetchone()
-      conexao.close()
-      return render_template("editar.html", dados=dados)
-
-@app.route("/apagar/<id_usuario>", methods=["POST"])
-def apagar(id_usuario):
-
-     conexao=sqlite3.connect("crud.db")
-     cursor=conexao.cursor()
-
-     cursor.execute(
-          "DELETE FROM usuarios WHERE id=?",
-          (id_usuario,),
-     )
-
-     conexao.commit()
-     conexao.close()
-     return redirect(url_for("usuarios"))
-
-           
-
-if __name__ == "__main__":
-      banco_novo()
-      app.run()
