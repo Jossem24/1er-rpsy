48a5c8a8b9190308be3fa5977e109682d289d14c
Jossem24 <paparilo024@hotmail.com>
2026-09-17 13:51:23 -0300

    CRUD validacao

--- src/1-SALA.py
+++ src/1-SALA.py
@@ -1135,6 +1135,8 @@ if __name__ == "__main__":
 #  </body>
 # </html>
 
+
+
 #Ponto 14
 
 import sqlite3
@@ -1239,3 +1241,100 @@ def apagar(id_usuario):
 if __name__ == "__main__":
       banco_novo()
       app.run()
+
+
+#    Ponto 15      
+
+
+import sqlite3
+from flask import Flask, render_template,request, redirect, url_for
+
+app = Flask(__name__)
+def banco_novo():
+   conexao = sqlite3.connect("validacao.db")
+   cursor = conexao.cursor()
+
+   cursor.execute(
+        "CREATE TABLE IF NOT EXISTS usuarios"
+        "(id INTEGER PRIMARY KEY AUTOINCREMENT,"
+        "nome TEXT,"
+        "idade INTEGER)"
+   )
+   conexao.commit()
+   conexao.close()
+@app.route("/cadastro", methods=["GET", "POST"])
+def cadastro():
+    if request.method == "POST":
+      nome = request.form.get("nome", "").strip()
+      idade_texto = request.form.get("idade", "").strip()
+
+      if not nome:
+        mensagem = "Digite um nome"
+        return render_template("cadastro.html", mensagem=mensagem)
+
+      try :
+         idade = int(idade_texto)
+      except ValueError:
+         mensagem = "Idade Invalida"
+         return render_template("cadastro.html", mensagem=mensagem)
+
+      if idade < 0 or idade >120:
+         mensagem = "Idade fora do intervalo"
+         return render_template("cadastro.html", mensagem=mensagem)
+   
+      conexao = sqlite3.connect("validacao.db")
+      cursor = conexao.cursor()
+      cursor.execute(
+
+         "INSERT INTO usuarios (nome, idade) VALUES (?,?)",
+         (nome, idade),
+      )
+      conexao.commit()
+      conexao.close()
+      return redirect(url_for("usuarios"))
+    return render_template("cadastro.html")
+
+
+@app.route("/usuarios")
+def usuarios():
+   conexao = sqlite3.connect("validacao.db")
+   cursor = conexao.cursor()
+
+   cursor.execute(
+
+       "SELECT nome, idade FROM usuarios"
+   )
+
+   dados = cursor.fetchall()
+   conexao.close()
+   return render_template("usuarios.html", dados=dados)
+if __name__=="__main__":
+   banco_novo()
+   app.run()
+# {% if mensagem%}
+#      <p> {{mensagem}} </p>
+# {%endif%}
+
+
+# <!DOCTYPE html>
+# <html lang="pt-br">
+#  <head>
+#     <title> Crud </title>
+#  </head>
+#  <body>
+#   <form action="/cadastro" method="post">
+#      <label for="nome">Nome: </label>
+#      <input type="text" id="nome" name="nome">
+
+#      <label for="idade">Idade: </label>
+#      <input type="number" id="idade" name="idade">
+
+#      <button type="submit">Enviar: </button>
+#   </form>
+#   </body>
+# </html>   
+   
+# {%for usuario in dados %}
+#     <p>nome : {{usuario  [0]}}</p>
+#     <p>idade : {{usuario [1]}}</p>
+# {%endfor%}      
--- src/2-TESTEO.py
+++ src/2-TESTEO.py
@@ -1,21 +1,66 @@
-from flask import Flask, render_template,request
+import sqlite3
+from flask import Flask, render_template,request, redirect, url_for
 
 app = Flask(__name__)
+def banco_novo():
+   conexao = sqlite3.connect("validacao.db")
+   cursor = conexao.cursor()
 
+   cursor.execute(
+        "CREATE TABLE IF NOT EXISTS usuarios"
+        "(id INTEGER PRIMARY KEY AUTOINCREMENT,"
+        "nome TEXT,"
+        "idade INTEGER)"
+   )
+   conexao.commit()
+   conexao.close()
 @app.route("/cadastro", methods=["GET", "POST"])
 def cadastro():
     if request.method == "POST":
       nome = request.form.get("nome", "").strip()
       idade_texto = request.form.get("idade", "").strip()
 
-      if not nome or not idade_texto:
-        mensagem = "Digite un nome"
+      if not nome:
+        mensagem = "Digite um nome"
         return render_template("cadastro.html", mensagem=mensagem)
 
       try :
          idade = int(idade_texto)
       except ValueError:
-         mensagem = "A idade precisa ser um numero"
+         mensagem = "Idade Invalida"
          return render_template("cadastro.html", mensagem=mensagem)
-    return render_template("cadastro.hmtl")
 
+      if idade < 0 or idade >120:
+         mensagem = "Idade fora do intervalo"
+         return render_template("cadastro.html", mensagem=mensagem)
+   
+      conexao = sqlite3.connect("validacao.db")
+      cursor = conexao.cursor()
+      cursor.execute(
+
+         "INSERT INTO usuarios (nome, idade) VALUES (?,?)",
+         (nome, idade),
+      )
+      conexao.commit()
+      conexao.close()
+      return redirect(url_for("usuarios"))
+    return render_template("cadastro.html")
+
+
+@app.route("/usuarios")
+def usuarios():
+   conexao = sqlite3.connect("validacao.db")
+   cursor = conexao.cursor()
+
+   cursor.execute(
+
+       "SELECT nome, idade FROM usuarios"
+   )
+
+   dados = cursor.fetchall()
+   conexao.close()
+   return render_template("usuarios.html", dados=dados)
+if __name__=="__main__":
+   banco_novo()
+   app.run()
+   
