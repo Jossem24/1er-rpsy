0954f8f7d0b0dc0a7c6e00770763bbb4197eda0a
Jossem24 <paparilo024@hotmail.com>
2026-09-21 13:47:58 -0300

    Jogo 80%

--- src/5-hanoi_grafico.py
+++ src/5-hanoi_grafico.py
@@ -43,24 +43,60 @@ def desenhar_discos():
             x = centro_x - largura // 2   #para o disco entra na haste
             y = 420 - (nivel + 1) * altura_disco 
 
+            cor = (45, 55, 75)
 
+            if (
+                indice_torre == torre_selecionada
+                and nivel == len(torre) - 1
+            ):
+                cor = (255, 185, 40)
             #desenho do disco
             pygame.draw.rect(
                 tela,
-                (45,55,75),
+                cor,
                 (x,y, largura, altura_disco - 4), #define o tamanho do rectangulo
                 border_radius=6, #arredonda cantos
             )   
+
+            texto_numero = fonte.render(
+                # str converte numero para texto
+                str(disco),
+                True,
+                (230, 230, 235),
+            )
+            area_numero = texto_numero.get_rect(
+                center=(
+                    #calculo para centrar o texto no retangulo do texto
+                    centro_x,
+                    y + (altura_disco - 4) // 2,
+                )
+            )    #mostra  o numero
+            tela.blit(texto_numero, area_numero)
+
+fonte = pygame.font.Font(None, 28)
 
 while executando:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             executando = False
+
+        #Detectar em que torre clicou o jogador    
                                    #MOUSEBU indica que o mouse foi clicado
         elif evento.type == pygame.MOUSEBUTTONDOWN and evento.button == 1:
             indice_clicado = evento.pos[0] // (LARGURA // 3)
-            print(indice_clicado)
-
+            if torre_selecionada is None :
+                if torres[indice_clicado]:
+                    torre_selecionada = indice_clicado
+                    print("Origem selecionada: ", torre_selecionada)
+            else:
+                if indice_clicado != torre_selecionada:
+                    if mover_disco(
+                        torres[torre_selecionada],
+                        torres[indice_clicado],
+                    ):
+                        movimientos += 1
+                        print("Movimentos:", movimientos)
+                torre_selecionada = None        
     tela.fill((15,15,18))
     COR_TORRE = (75,85,105)
 
@@ -81,6 +117,20 @@ while executando:
             6,
         )
     desenhar_discos()
+    for indice, letra in enumerate (("A", "B", "C")):
+        texto_letra = fonte.render(letra, True, (230, 230, 235))
+                                 #get_rect cria uma area centralizada e centra a letra na barra(haste)
+        area_texto = texto_letra.get_rect(
+            center=(posicoes_x[indice], 455)
+        )    #blit mostra a letra na area centralizada
+        tela.blit(texto_letra, area_texto)
+    texto_movimentos = fonte.render(  #render transforma texto em imagem
+        f"Movimentos: {movimientos}",
+        True,
+        (230, 230, 235),
+    )
+         #blit desenha uma imagem na tela, na posição colocada (20,20)    
+    tela.blit(texto_movimentos, (20,20))
     pygame.display.flip()
     relogio.tick(60)
 
